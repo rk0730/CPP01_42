c27c86ba12e6238c3b883f9c2d9566d09e709ca4
北尾遼馬 <kitaoryoma@kitaoryoubanoMacBook-Air.local>
2025-01-18 17:57:02 +0900

    randomChump

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,16 +1,20 @@
 #include "Zombie.hpp"
 
 Zombie *newZombie(std::string name);
+void randomChump(std::string name);
 
-int main(void){
+int main(void)
+{
 	Zombie zombie1("Zombie1");
 	Zombie zombie2("Zombie2");
 	zombie1.announce();
 	zombie2.announce();
-	
+
 	Zombie *zombie3 = newZombie("Zombie3");
 	zombie3->announce();
 	delete zombie3;
+
+	randomChump("Zombie4");
 
 	return 0;
 }
