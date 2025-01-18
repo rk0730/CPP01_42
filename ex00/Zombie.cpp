e35f131950ce09a5cf227a0cb0cbbbff8453ea62
北尾遼馬 <kitaoryoma@kitaoryoubanoMacBook-Air.local>
2025-01-18 17:43:15 +0900

    ex00導入

---
+++ ex00/Zombie.cpp
@@ -0,0 +1,16 @@
+#include "Zombie.hpp"
+
+Zombie::~Zombie()
+{
+	std::cout << "Zombie " << name << " is dead" << std::endl;
+}
+
+void Zombie::announce(void)
+{
+	std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
+void Zombie::setName(std::string name)
+{
+	this->name = name;
+}
