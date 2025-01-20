a64007168e37130d1ce15f550982e53b40f0c641
北尾遼馬 <kitaoryoma@kitaoryoubanoMacBook-Air.local>
2025-01-20 18:24:49 +0900

    Weapon.cpp作成

---
+++ ex03/Weapon.hpp
@@ -0,0 +1,17 @@
+#ifndef WEPON_HPP
+#define WEPON_HPP
+
+#include <iostream>
+
+class Weapon
+{
+public:
+	Weapon(std::string type);
+	const std::string &getType() const;
+	void setType(std::string type);
+
+private:
+	std::string _type;
+};
+
+#endif
