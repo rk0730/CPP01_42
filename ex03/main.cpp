c8c00356a85f7facf4bc9b6663825d6396dd49c5
北尾遼馬 <kitaoryoma@kitaoryoubanoMacBook-Air.local>
2025-01-20 19:19:21 +0900

    HumanA作成

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -1,9 +1,14 @@
-#include "Weapon.hpp"
+#include "HumanA.hpp"
 
 int main(void){
 	Weapon club = Weapon("crude spiked club");
 	std::cout << club.getType() << std::endl;
+	
+	HumanA bob("Bob", club);
+	bob.attack();
 	club.setType("some other type of club");
+	bob.attack();
+
 	std::cout << club.getType() << std::endl;
 	return 0;
 }
